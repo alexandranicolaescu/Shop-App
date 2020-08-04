47af45cdab275f0e02c83f3832f56f2dccf2923d
Mon Alexandra <alexandramonicanicolaescu@gmail.com>
2020-08-04 15:33:00 +0300

    Deleting Items from Admin screen + Cart logistic

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {FlatList, Button, Platform} from 'react-native';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import {HeaderButtons, Item} from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
 import ProductItem from '../../components/shop/ProductItem';
 import Colors from '../../constants/Colors';
+import * as productsActions from '../../store/actions/products';
 
 const UserProductScreen = props => {
     const userProducts = useSelector(state => state.products.userProducts);
+    const dispatch = useDispatch();
 
     return (
         <FlatList
@@ -33,6 +35,7 @@ const UserProductScreen = props => {
                         color = {Colors.primary}
                         title = "Delete"
                         onPress = {() => {
+                            dispatch(productsActions.deleteProduct(itemData.item.id));
                         }}
                     />
                 </ProductItem>
