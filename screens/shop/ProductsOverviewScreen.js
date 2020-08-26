4c6a7456cb185f6e88d1b7f0eff2d85704387af6
Mon Alexandra <alexandramonicanicolaescu@gmail.com>
2020-08-26 13:24:17 +0300

    Navigation Listener for All Products Screen

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -23,6 +23,7 @@ const ProductsOverviewScreen = props => {
     const dispatch = useDispatch();
 
     const loadProducts = useCallback(async() => {
+        //console.log('LOAD PRODUCTS');
         setError(null);
         setIsLoading(true);
         try {
@@ -32,6 +33,14 @@ const ProductsOverviewScreen = props => {
         }
         setIsLoading(false);
     }, [dispatch, setIsLoading, setError]);
+
+    useEffect(() => {
+        const willFocusSub = props.navigation.addListener('willFocus', loadProducts);
+        return () => {
+            willFocusSub.remove();
+        };
+    }, [loadProducts]);
+    
 
     useEffect(() => {
         loadProducts();
